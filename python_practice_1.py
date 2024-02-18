7131cbce548e704e337d4bab758f6953295636b3
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2024-02-18 20:45:20 +0200

    add pr3

--- python_practice_1.py
+++ python_practice_1.py
@@ -1,1 +1,17 @@
 print("Artemii Kolomiichuk, IPZ_Group_7")
+
+import random
+
+NUMBER = random.randint(1, 100)
+guess = -1
+guesses = 1
+while guess != NUMBER:
+    guess = int(input())
+    if guess > NUMBER:
+        print("Менше")
+        guesses += 1
+    elif guess < NUMBER:
+        print("Більше")
+        guesses += 1
+    else:
+        print(f"Кількість спроб: {guesses}")
